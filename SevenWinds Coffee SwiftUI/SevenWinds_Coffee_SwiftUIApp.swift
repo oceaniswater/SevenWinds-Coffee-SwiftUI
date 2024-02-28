cd33c51719ba8e0559222d89136dac079dc99971
oceaniswater <markflyingbear@gmail.com>
2024-02-28 16:46:05 +0000

    Initial Commit

---
+++ SevenWinds Coffee SwiftUI/SevenWinds_Coffee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenWinds_Coffee_SwiftUIApp.swift
+//  SevenWinds Coffee SwiftUI
+//
+//  Created by Mark Golubev on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SevenWinds_Coffee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
